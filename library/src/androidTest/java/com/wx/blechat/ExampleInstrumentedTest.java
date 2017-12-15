749fdf86d95fbcfe135dc733623e6867c50cb197
wangxin <xinyer0510@gmail.com>
2017-12-15 16:26:55 +0800

    put code to lib.

--- library/src/androidTest/java/com/wx/blechat/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/wx/blechat/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.wx.blechat", appContext.getPackageName());
+        assertEquals("com.wx.blechat.test", appContext.getPackageName());
     }
 }
